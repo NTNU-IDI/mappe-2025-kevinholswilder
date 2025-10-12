1c1be81fb741ff32630d98266d10187898b025d6
Kevin Holswilder <kevin@holswilder.no>
2025-10-12 18:37:34 +0200

    Edited Main.java, which now contains a simple object of ConsoleUI with its two main functions.

--- src/main/java/edu/ntnu/iir/bidata/Main.java
+++ src/main/java/edu/ntnu/iir/bidata/Main.java
@@ -1,8 +1,19 @@
 package main.java.edu.ntnu.iir.bidata;
 
+import main.java.edu.ntnu.iir.bidata.ui.ConsoleUI;
+
+/**
+ * Main class
+ *
+ * @Author Kevin Holswilder
+ * @Date 12/10/2025
+ */
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ConsoleUI consoleUI = new ConsoleUI();
+        consoleUI.init();
+        consoleUI.start();
     }
 }
