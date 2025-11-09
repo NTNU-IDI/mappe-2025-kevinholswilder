d343098636db630fb76ca29fca2ad596bdcec85c
Kevin Holswilder <kevin@holswilder.no>
2025-11-09 13:38:16 +0100

    Updated menu text.

--- src/main/java/edu/ntnu/iir/bidata/flow/MenuDisplay.java
+++ src/main/java/edu/ntnu/iir/bidata/flow/MenuDisplay.java
@@ -13,18 +13,18 @@ public class MenuDisplay {
      * Displays the main menu.
      */
     public static void mainMenu() {
-        System.out.println("--=[ Main Menu ]=--");
-        System.out.println("Welcome to the diary application,\nplease select one of the options.");
+        System.out.println("--=[ Cooking Diary ]=--");
+        System.out.println("Welcome to the cooking diary,\nplease select one of the options below.");
         System.out.println("1. Log-in.");
         System.out.println("2. Register a new author.");
         System.out.println("3. Exit the program.");
     }
 
     /**
-     * Displays the diary menu.
+     * Displays the cooking diary menu.
      */
     public static void diaryMenu() {
-        System.out.println("--=[ Diary Menu ]=--");
+        System.out.println("--=[ Cooking Diary ]=--");
         System.out.println("1. Write a new diary entry.");
         System.out.println("2. Delete an existing diary entry.");
         System.out.println("3. Search for diary entries.");
@@ -36,7 +36,7 @@ public class MenuDisplay {
      * Displays the search menu.
      */
     public static void searchDiaryMenu() {
-        System.out.println("--=[ Search Menu ]=--");
+        System.out.println("--=[ Cooking Diary ]=--");
         System.out.println("1. Search diary by title.");
         System.out.println("2. Search diaries by date.");
         System.out.println("3. Search diaries by period.");
