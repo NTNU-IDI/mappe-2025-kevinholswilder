6d24137eb87515b3ca6902d0ae3b6611522a7d3a
Kevin Holswilder <kevin@holswilder.no>
2025-11-30 20:30:17 +0100

    Implemented helper-methods for formatting and parsing RecipeLabel objects.

--- src/main/java/edu/ntnu/iir/bidata/enumerations/RecipeLabel.java
+++ src/main/java/edu/ntnu/iir/bidata/enumerations/RecipeLabel.java
@@ -1,6 +1,9 @@
 package main.java.edu.ntnu.iir.bidata.enumerations;
 
 import main.java.edu.ntnu.iir.bidata.models.DiaryEntry;
+import main.java.edu.ntnu.iir.bidata.utils.UtilityManager;
+
+import java.util.Collection;
 
 /**
  * This class contains all the possible labels for the labels of a {@link DiaryEntry}.
@@ -17,5 +20,40 @@ public enum RecipeLabel {
     HALAL,
     KETO,
     EGGFREE,
-    SOYFREE
+    SOYFREE;
+
+    /**
+     * Returns null if the input is not a valid label.
+     *
+     * @param input the user input.
+     * @return the {@link RecipeLabel} if valid, null otherwise.
+     */
+    public static RecipeLabel tryParse(String input) {
+        try {
+            return valueOf(input.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
+    }
+
+    /**
+     * Prints all the labels in the collection.
+     *
+     * @param labels the collection of labels.
+     */
+    public static void printLabels(Collection<RecipeLabel> labels) {
+        for (RecipeLabel label : labels) {
+            System.out.println(" - " + label.formattedLabel());
+        }
+    }
+
+    /**
+     * Returns the label in a formatted way.
+     *
+     * @return the formatted label.
+     */
+    public String formattedLabel() {
+        return UtilityManager.capitalize(this.name().toLowerCase());
+    }
+
 }
